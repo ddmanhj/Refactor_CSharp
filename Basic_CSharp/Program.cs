403d5e039ddc08d7c313adc9cddf8f00557c177c
manhj <manhdd@vnisc.com.vn>
2025-04-03 14:30:01 +0700

    update 26 Variable Definition, Initializtion and Assigning

--- Basic_CSharp/Program.cs
+++ Basic_CSharp/Program.cs
@@ -1,36 +1,67 @@
-﻿//Understand User Input and the Data Type String
+﻿Console.WriteLine("Enter the first number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {firstNumber + secondNumber}");
+Console.ReadKey();
 
+
+/*
+// 26 Variable Definition, Initializtion and Assigning
+// defining or setting up a variable
+string petsName;
+
+// initialize variable
+petsName = "Milo";
+Console.WriteLine($"my pet is {petsName}");
+petsName = "Barky";
+Console.WriteLine($"my pet is {petsName}");
+Console.ReadKey();
+*/
+
+/*
+//23. Overview of Data Types
+// Referency Type
+string myName = "Manhj";
+
+// Value Type
+int myAge = 35;
+double pi = 3.14;
+
+byte age = 255;
+
+short linkdinConnections = 32550;
+
+long phoneNumber = 017547723991;
+
+float pi2 = 3.14f;
+*/
+
+/*
+//Understand User Input and the Data Type String
 //Prints out whatever is in side of ()
 Console.WriteLine("What is your name?");
-
 Console.Beep();
-
 // Variable with the name "nameUser" and the data type "string"
 // strings are used for text
 string nameUser = Console.ReadLine() ?? "";
 Console.WriteLine("You entered " + nameUser);
-
 // takes the user input and stores it
 Console.ReadKey();
-
+*/
 
 
 /*
 //15.  Using variables - String
 Console.WriteLine("Hello, World");
-
 //declare a (string) variable
 string myName;
 // assign a value to the myFriendsName variable 
 myName = "manhj";
-
 // use/access the variable
 Console.WriteLine(myName);
-
 myName = "Manh";
-
 Console.WriteLine(myName);
-
 //declare a variable and assign a value to it.
 string myName2 = "Manhj";
 Console.ReadKey();
