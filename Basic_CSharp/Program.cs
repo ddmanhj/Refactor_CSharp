376a719c8575b548e9e7a9dde6e1e908d3f25a51
manhj <manhdd@vnisc.com.vn>
2025-04-03 17:25:52 +0700

    update study lession 36

--- Basic_CSharp/Program.cs
+++ Basic_CSharp/Program.cs
@@ -1,13 +1,45 @@
-﻿Console.WriteLine("Enter the first number:");
+﻿/*
+// implicit conversion
+int myInput = 1348912084;
+double myDouble = myInput;
+
+long myLong = myInput;
+
+myLong = 39102948140145320;
+
+float myFloat = 123.123f;
+myDouble = myFloat;
+*/
+
+/*35.Fixing some more bugs
+//datatype variableName = initial value;
+double myNumber = 0;
+double myNumber2 = 0;
+Console.WriteLine("Enter a whole number!");
+
+string firstNumber = Console.ReadLine();
+myNumber = double.Parse(firstNumber);
+
+Console.WriteLine("Enter a whole number!");
+firstNumber = Console.ReadLine(); 
+myNumber2 = double.Parse(firstNumber);
+
+double sum = Math.Round((myNumber + myNumber2), 2);
+
+// we are adding to strings together and writing them onto the console
+Console.WriteLine($"The result of {myNumber} and {myNumber2} is {sum}" );
+*/
+
+/* My solution 🧠
+Console.WriteLine("Enter the first number:");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the second number:");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {firstNumber + secondNumber}");
-Console.ReadKey();
-
+*/
 
 /*
-// 26 Variable Definition, Initializtion and Assigning
+// 26 Variable Definition, Initialization and Assigning
 // defining or setting up a variable
 string petsName;
 
@@ -16,7 +48,6 @@ petsName = "Milo";
 Console.WriteLine($"my pet is {petsName}");
 petsName = "Barky";
 Console.WriteLine($"my pet is {petsName}");
-Console.ReadKey();
 */
 
 /*
@@ -30,7 +61,7 @@ double pi = 3.14;
 
 byte age = 255;
 
-short linkdinConnections = 32550;
+short linkInConnections = 32550;
 
 long phoneNumber = 017547723991;
 
@@ -47,7 +78,6 @@ Console.Beep();
 string nameUser = Console.ReadLine() ?? "";
 Console.WriteLine("You entered " + nameUser);
 // takes the user input and stores it
-Console.ReadKey();
 */
 
 
@@ -64,6 +94,6 @@ myName = "Manh";
 Console.WriteLine(myName);
 //declare a variable and assign a value to it.
 string myName2 = "Manhj";
-Console.ReadKey();
-
 */
+
+Console.ReadKey();
