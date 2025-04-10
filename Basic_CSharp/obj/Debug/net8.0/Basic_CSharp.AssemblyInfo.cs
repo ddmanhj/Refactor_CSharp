ae211342bd3217477ef6f541bfec908e2fd50cc4
Manhj <ddmanhj@gmail.com>
2025-04-10 22:24:56 +0700

    update lession Relational Operators

--- Basic_CSharp/obj/Debug/net8.0/Basic_CSharp.AssemblyInfo.cs
+++ Basic_CSharp/obj/Debug/net8.0/Basic_CSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basic_CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89312c44cb7323b7804cd6c77f4051bda1182a80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+376a719c8575b548e9e7a9dde6e1e908d3f25a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basic_CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basic_CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ MakingDecisions/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1 = 0;
+int num2 = 0;
+
+
+bool isEqual = num1 == num2;
+bool isNotEqual = num1 != num2;
+
+
+Console.WriteLine("Please enter a whole number");
+if (num1 == int.Parse(Console.ReadLine()))
+{
+	Console.WriteLine("Numbers are equal!");
+	int age = int.Parse(Console.ReadLine());
+	if (age >= 18)
+	{
+        Console.WriteLine("Please enter your address, " + "so that we can send you the price!");
+        string address = Console.ReadLine();
+		
+	}
+	else
+	{
+        Console.WriteLine("Sorry, you can't get your price due to your age!");
+    }
+}
+else
+{
+    Console.WriteLine("Numbers aren't equal");
+}
+
+/*
+// Relational Operators < <= > >= == !=
+Console.WriteLine("How old are you?");
+int ageUser = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("Are you going with your parents?");
+bool isWithParents = Convert.ToBoolean(Console.ReadLine());
+
+if (ageUser >= 13 && isWithParents)
+{
+	Console.WriteLine("Go party in the club with your parents!");
+}
+else if (ageUser > 18)
+{
+	Console.WriteLine("Go party in the club!");
+
+}
+else
+{
+	Console.WriteLine("Go party in school!");
+}
+*/
+
+/*
+bool isRainy = true;
+bool hasUmbrella = true;
+
+// Logical Operators
+// AND &&
+// OR ||
+// NOT !
+
+// Variants of OR statements
+// true && true => True
+// true && false => False
+// false && true => False
+// false && false => False
+
+if (isRainy && hasUmbrella)
+{
+    Console.WriteLine("I'M not getting wet!");
+}
+Console.WriteLine("Ay OK!");
+*/
+
+
+Console.ReadKey();
