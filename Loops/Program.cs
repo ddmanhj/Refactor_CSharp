a4850cd039d119b821af56ee9db7f528e917a7b9
manhj <manhdd@vnisc.com.vn>
2025-04-15 17:58:59 +0700

    update code

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,5 +1,99 @@
 ﻿using Loops;
 
+// TwoDimensional Arrays (Mảng 2 chiều)
+int[,] array2DDeclaration = new int[3, 3];
+// [0] [0] [0]
+// [0] [0] [0]
+// [0] [0] [0]
+
+int[,,] array3DDeclaration = new int[3, 3, 3];
+
+int[,] array2DInizilized = { { 1, 2 }, { 3, 4 } };
+// [1] [2]
+// [3] [4]
+
+Console.WriteLine(array2DInizilized[0,0]); // 1
+
+array2DInizilized
+
+/*
+//Array part 2 
+// declare an array and set the array element values
+int[] myIntArray = { 1, 14, 16, 20, 25 };
+string[] weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+Console.WriteLine("Length of weekdays Arrays is " + weekDays.Length);
+
+for (int i = 0; i < weekDays.Length; i++)
+{
+    //Console.WriteLine("Weekday " + (i + 1) + " is " + weekDays[i]);
+}
+
+foreach (var day in weekDays)
+{
+    Console.WriteLine(day);
+}
+*/
+
+/*
+// Simple Array
+int[] myIntArray = new int[5];
+
+myIntArray[0] = 1;
+myIntArray[1] = 14;
+myIntArray[2] = 16;
+myIntArray[3] = 20;
+myIntArray[4] = 25;
+
+// Indexs   [0][1][2][3][4]
+// Content  [1][14][16][20][25]
+*/
+
+/*
+// break and continue
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 3)
+    {
+        continue; // Bỏ qua đoạn code này và tiếp tục vòng lặp
+        Console.WriteLine("I've had enough!");
+        // break; // Thoát khỏi vòng lặp ngay lập tức
+
+    }
+
+    Console.WriteLine("Only if not continued");
+}
+*/
+
+/*
+// Do while Loop
+int currentScore;
+int sum = 0;
+int counter = 0;
+// do-while loop is a  post-test loop
+
+do
+{
+    Console.WriteLine("Enter your students score. Enter -1 to finish!");
+    currentScore = int.Parse(Console.ReadLine());
+    if (currentScore != -1)
+    {
+        sum = sum + currentScore;
+        counter++;
+    }
+
+
+} while (currentScore != -1);
+
+int average = sum / counter;
+Console.WriteLine("The average is: " + average);
+*/
+
+//var textAdventureGame = new TextAdventureGame();
+//textAdventureGame.TextAdventureGameProgram();
+
+/*
 Random random = new Random();
 int secretNumber = random.Next(1, 101);
 int userGuess = 0;
@@ -24,6 +118,7 @@ while (userGuess != secretNumber)
         Console.WriteLine("Congratulations! You guessed the number! It took you " + counter);
     }
 }
+*/
 
 /*
 // The While Loop
