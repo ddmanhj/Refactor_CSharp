8588aa7d1588e6fd42cab5ee648c9902b2787519
Manhj <ddmanhj@gmail.com>
2025-05-15 22:51:35 +0700

    update class and variable member OOP

---
+++ OOP/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	// It's a internal, which means that it can only be accessed within the same assembly.
+
+	internal class Car
+	{
+		// member variable
+		// private hides the variable from other classes
+		private string _model = "";
+
+		private string _brand = "";
+		// Constructor
+		public Car(string model, string brand) {
+			_model = model;
+            Console.WriteLine($"A {brand} of the model {_model} has been created");
+        }
+	}
+}
