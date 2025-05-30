b7b3debd2729dd1dd085162cbef49cbcb1e42781
manhj <manhdd@vnisc.com.vn>
2025-05-30 16:01:59 +0700

    Defining property is set

--- OOP/Car.cs
+++ OOP/Car.cs
@@ -6,25 +6,44 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
-	// It's a internal, which means that it can only be accessed within the same assembly.
+    // It's a internal, which means that it can only be accessed within the same assembly.
 
-	internal class Car
-	{
+    internal class Car
+    {
         // member variable
         // private hides the variable from other classes
         private string _model = "";
+        private string _brand = "";
+        private bool _isLuxury;
         //Property
         public string Model { get => _model; set => _model = value; }
-        private string _brand = "";
-        public string Brand { get => _brand; set => _brand = value; }
 
-		// Constructor
-		public Car(string model, string brand) {
-            Brand = brand;
-            Model = model;
-            Console.WriteLine($"A {Brand} of the model {Model} has been created");
+        public string Brand
+        {
+            get => _brand;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("You entered NOTHING!");
+                    _brand = "Unknown"; // Default value if nothing is entered
+                }
+                else
+                {
+                    _brand = value;
+                }
+            }
         }
 
-      
+        public bool IsLuxury { get => _isLuxury; set => _isLuxury = value; }
+
+        // Constructor
+        public Car(string model, string brand, bool isLuxury)
+        {
+            Brand = brand;
+            Model = model;
+            _isLuxury = isLuxury;
+            Console.WriteLine($"A {Brand} of the model {Model} has been created");
+        }
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,8 +6,16 @@
         {
             // Create an object of the Car class
             // Creating an instace of the Class Car
-            Car audi = new Car("A3", "Audi");
-            Car bmw = new Car("i7", "BMW");
+            Car audi = new Car("A3", "Audi", false);
+            Car bmw = new Car("i7", "BMW", true);
+
+            Console.WriteLine("Please enter the Brand name");
+
+            // "SETING BRAND"
+            audi.Brand = Console.ReadLine();
+
+            // "GETTING BRAND"
+            Console.WriteLine("Brand is: " + audi.Brand);
 
             Console.ReadKey();
         }
